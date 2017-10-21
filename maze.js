28884ba36bd0d2a504311b0b11982564fe519faf
Unknown <mario_braham@hotmail.com>
2017-10-20 23:54:42 -0500

    Excercise 2

--- maze.js
+++ maze.js
@@ -1,6 +1,18 @@
 window.onload = function(){
-	let wall = document.getElementById("boundary1");
-	wall.addEventListener("mouseover",function(){
-		wall.setAttribute("class","boundary youlose");
+	let walls = document.querySelectorAll(".boundary");
+	let hit_wall = false;
+	for(let index = 0; index < walls.length-1; index++){
+		walls[index].addEventListener("mouseover",function(){
+			walls[index].setAttribute("class","boundary youlose");
+			hit_wall = true;
+		});
+	}
+	let maze_game = document.getElementById("maze");
+	maze_game.addEventListener("mouseover",function(){
+		if(hit_wall === true){
+			for(let index = 0; index < walls.length-1; index++){
+				walls[index].setAttribute("class","boundary youlose");
+			}
+		}
 	});
 };
